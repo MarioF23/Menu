217f6cd0703537a4554b12cd9a4f2629397097bf
MarioF23 <90580680+MarioF23@users.noreply.github.com>
2021-09-12 20:42:33 -0600

    Add files via upload

---
+++ Menu Mario.cpp
@@ -0,0 +1,298 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+		int Mario;
+		do{ 
+		system("cls");
+		system("COLOR F0");
+		cout<<"\t\tBIENVENIDO AL MENU\n"<<endl;	
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;	
+		cout<<"\t\tPROGRAMAS\n"<<endl;	
+cout<<"1.-Conversion de numeros arabigos a romanos (minimo 1,000)\n"<<endl;	
+
+cout<<"2.-Determinar si un numero es par o impa\n"<<endl;  
+
+cout<<"3.-Convertir de kilometros a millas, metros a pulgadas, de libras a kilos y viceversa\n"<<endl;
+
+cout<<"4.-Determinar si una palabra o un numero es palindromo.\n"<<endl; 
+
+cout<<"5.-Conversion de numeros arabigos a romanos (minimo 1,000)\n"<<endl;
+
+cout<<"6.-Conversion de numeros enteros a letras\n"<<endl;
+
+cout<<"7.-Conversion de numeros enteros con decimal a letras\n"<<endl;
+
+cout<<"8.-Una tabla de multiplicar\n"<<endl;
+
+cout<<"9.-Todas las tablas de multiplicar del 1 al 10\n"<<endl;
+
+cout<<"10.-Crear de forma grafica la multiplicacion manual\n"<<endl;
+
+cout<<"11-Conversion de numeros decimales a binario\n"<<endl;
+
+cout<<"12.-Conversion de numeros decimales a hexadecimales\n"<<endl;	
+
+cout<<"13.-Crear Figuras Geometricas Basicas\n"<<endl;
+
+cout<<"14.-Mover un punto en toda la pantalla\n"; 
+cout<<"(W)arriba (S)abajo (A)izquierda (D)derecha (C)cerrar\n"<<endl;
+
+cout<<"15.-Simular un Cajero Automatico \n"<<endl;
+
+cout<<"16.-Calcular la hipotenusa \n"<<endl;
+
+cout<<"17.numero mayor o menor-\n"<<endl;
+
+cout<<"18.-calcular promedio de edades\n"<<endl;
+
+cout<<"19.-sueldo por horas trabajadas\n"<<endl;
+
+cout<<"20.-dias del mes\n"<<endl;
+
+cout<<"25.-salir \n"<<endl;
+		cin>>Mario;
+		
+system("cls");
+
+switch(Mario) { 
+	case 1:
+	system("cls");
+	system("n1.exe"); cout<<"\n"<<endl; main();
+ 		 		
+	system("cls");
+			
+
+break;	
+		
+		case 2:
+	system("cls");
+	system("n2.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+	system("cls");
+			
+
+break;		
+		
+		
+	case 3:
+	system("cls");
+	system("n3.exe"); cout<<"\n"<<endl; main();
+ 		 		
+    system("pause");
+	system("cls");
+			
+
+break;
+		
+		
+		
+			case 4:
+	system("cls");
+	system("n4.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+	system("cls");
+			
+
+break;	
+		
+		
+	case 5:
+system("cls");
+	system("n5.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;		
+		
+		
+	case 6:
+system("cls");
+	system("n6.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+	case 7:
+system("cls");
+	system("n7.exe"); cout<<"\n"<<endl; main(); //no esta hecho aun
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 8:
+system("cls");
+	system("n8.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 9:
+system("cls");
+	system("n9.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 10:
+system("cls");
+	system("n10.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 11:
+system("cls");
+	system("n11.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+
+
+	case 12:
+system("cls");
+	system("n12.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 13:
+system("cls");
+	system("n13.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+
+	case 14:
+system("cls");
+	system("n14.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 15:
+system("cls");
+	system("n15.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 16:
+system("cls");
+	system("n16.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 17:
+system("cls");
+	system("n17.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 18:
+system("cls");
+	system("n18.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+
+	case 19:
+system("cls");
+	system("n19.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;
+
+		
+		
+	case 20:
+system("cls");
+	system("n20.exe"); cout<<"\n"<<endl; main();
+ 		 		
+system("pause");
+system("cls");
+			
+
+break;		
+		
+		
+		
+		
+		
+		
+		case 25:
+		exit(1); 
+		break;
+		
+		
+		
+		break;
+	 } 
+}while (Mario!=21);
+	return 0;
+}
